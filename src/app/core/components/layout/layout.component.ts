c6307e3b2f7e4444c7bce0d30ffe160d3687dd45
Hetty de Vries <hetty82@gmail.com>
2018-04-13 13:46:20 +0200

    Add some editor settings and base styles

--- src/app/core/components/layout/layout.component.ts
+++ src/app/core/components/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Input } from '@angular/core'
 
 @Component({
   selector: 'app-layout',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./layout.component.sass']
 })
 export class LayoutComponent implements OnInit {
+  @Input() title: string
+  @Input() subTitle: string
 
   constructor() { }
 
